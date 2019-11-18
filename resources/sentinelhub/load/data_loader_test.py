6b22d56172c15e610328f965918cf98e3f959c87
c16192 <ishidashu@gmail.com>
2019-11-18 09:28:41 +0000

    modified data loader test

--- resources/sentinelhub/load/data_loader_test.py
+++ resources/sentinelhub/load/data_loader_test.py
@@ -1,4 +1,4 @@
-from resources.sentinelhub.load.data_loader import SentinelHubDetaLoader
+from .data_loader import SentinelHubDetaLoader
 from sentinelhub import CRS, BBox
 
 loader = SentinelHubDetaLoader()
@@ -8,9 +8,9 @@ betsiboka_bbox = BBox(bbox=betsiboka_coords_wgs84, crs=CRS.WGS84)
 info = {
     "layer": 'TRUE-COLOR-S2-L1C',
     "bbox": betsiboka_bbox,
-    "time": ('2017-12-01','2017-12-31'),
-    "resx": "10m",
-    "resy": "10m"
+    "time": ('2019-10-01','2019-10-31'),
+    "resx": "60m",
+    "resy": "60m"
 }
 
 loader.load(info)
