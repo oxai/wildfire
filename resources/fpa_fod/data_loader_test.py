bbddb443d7d6f929b2030ddc9e6b546dc89cd5f2
c16192 <ishidashu@gmail.com>
2019-11-22 23:02:40 +0000

    Fixed test

--- resources/fpa_fod/data_loader_test.py
+++ resources/fpa_fod/data_loader_test.py
@@ -2,5 +2,6 @@ from .data_loader import FpaFodDataLoader
 
 
 loader = FpaFodDataLoader()
-df = loader.get_records(lat=45, lng=-116, loc_delta=5, from_date="2014-01-01", until_date="2015-01-01", min_fire_size=5.0)
+loc = {"lat": 45, "lng": -116, "delta": 5}
+df = loader.get_records(loc, from_date="2014-01-01", until_date="2015-01-01", min_fire_size=5.0)
 print(df)
