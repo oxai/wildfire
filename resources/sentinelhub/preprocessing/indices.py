289ee753ffd1c3ad99e767673465b366bb862006
c16192 <ishidashu@gmail.com>
2019-11-25 23:30:11 +0000

    Fixed formatting

--- resources/sentinelhub/preprocessing/indices.py
+++ resources/sentinelhub/preprocessing/indices.py
@@ -1,10 +1,11 @@
 from numpy import divide, nan_to_num
 
+
 def get_nbr(image):
 
     B08 = image[:, :, 7]
     B12 = image[:, :, 11]
-    img = divide(B08-B12, B08+B12)
+    img = divide(B08 - B12, B08 + B12)
     img = nan_to_num(img)
     
-    return(img)
+    return img
