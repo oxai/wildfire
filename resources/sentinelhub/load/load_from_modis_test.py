418ba23cdbd18210f83f8837b833fc4256d63fec
c16192 <ishidashu@gmail.com>
2019-12-03 09:21:26 +0000

    Modified modis test

--- resources/sentinelhub/load/load_from_modis_test.py
+++ resources/sentinelhub/load/load_from_modis_test.py
@@ -2,4 +2,4 @@ from .load_from_modis import SentinelLoaderFromModis
 
 
 loader = SentinelLoaderFromModis()
-loader.download('TRUE-COLOR-S2-L1C', loc=None, from_date='2018-01-01', until_date=None)
+loader.download('BANDS-S2-L2A', loc=None, from_date='2018-01-01', until_date=None, max_cloud_coverage=0.1, subdir="modis_fire")
