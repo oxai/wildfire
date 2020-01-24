b09768d90d2db54694e847791d09aa62113d82cf
c16192 <ishidashu@gmail.com>
2020-01-24 09:55:00 +0000

    Fixed test for tile_loader

--- resources/gee/tile_loader_test.py
+++ resources/gee/tile_loader_test.py
@@ -1,8 +1,8 @@
 import ee
 from .tile_loader import GeeTileLoader, TileQuery
 from .methods import get_ee_product
-from skimage import io
 from .config import EE_CREDENTIALS
+import matplotlib.pyplot as plt
 
 
 ee.Initialize(EE_CREDENTIALS)
@@ -10,13 +10,14 @@ ee.Initialize(EE_CREDENTIALS)
 loader = GeeTileLoader()
 
 ee_product = get_ee_product(
-    platform="landsat",
-    sensor="8",
-    product="raw"
+    platform="modis",
+    sensor="terra",
+    product="temperature"
 )
 
-query = TileQuery(x=2, y=2, z=3, date_from="2019-12-01", date_to="2019-12-30", reducer="median")
+query = TileQuery(x=2, y=3, z=3, date_from="2019-12-01", date_to="2019-12-30", reducer="median")
 
 out = loader.visualise(ee_product, query)
 print(out)
-io.imsave("file.jpg", out)
+plt.imshow(out)
+plt.show()
