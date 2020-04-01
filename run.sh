5fab475673f83d2d5eb4b59fb66b84356f0243e7
Jan Malinowski <malinowwskijasiek1999@gmail.com>
2020-04-01 20:23:18 +0000

    Delete run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-export TRAINING_DATA=input/train_folds.csv
-export TEST_DATA=input/test.csv
-export c=2
-export ROOT_DIR=input/data
-export N_EPOCHS=1
-export LR=0.005
-export MODEL=$1
-export BS=16
-python -m src.train
