b152efadd2b20637e220b3ebe4532d829b39ee4a
Lou1sM <oneillml@tcd.ie>
2020-05-02 15:00:11 +0000

    add already_normalized argument to vis_indicators, delete duplicate get_conf_** functions

--- resources/gee/vis_handler.py
+++ resources/gee/vis_handler.py
@@ -63,6 +63,16 @@ def vis_nbr(ee_product, image):
     return out
 
 
+@vis_handler_wrapper
+def vis_firethresh(ee_product, image):
+    swir, swir2, mask = get_bands_by_name(ee_product, image, ['SWIR', 'SWIR2', 'cloud_mask'])
+    out = apply_palette((swir + swir2) / 4, [
+        'black', 'red', 'yellow'
+    ])
+    out[-1] = mask
+    return out
+
+
 def get_natural_nirswirmix(blue, green, red, nir, swir2):
     return [stretch((2.1 * red + 0.5 * swir2), 0.01, 0.99), stretch((2.2 * green + 0.5 * nir), 0.01, 0.99),
             stretch(3.2 * blue, 0.01, 0.99)]
@@ -84,19 +94,19 @@ def get_veg_levels(blue, green, red, nir, swir2):
 
 
 # Functions to compute masks from metrics
-def get_fire_indicator(ee_product, image, sensitivity=1.0):
+def get_fire_indicator(ee_product, image, already_normalized, sensitivity=1.0):
     swir, swir2 = get_bands_by_name(ee_product, image, ['SWIR', 'SWIR2'])
     # Increase sensitivity for more possible fires and more wrong indications
     return (swir + swir2) * sensitivity
 
 
-def get_veg_indicator(ee_product, image, sensitivity=1.0):
+def get_veg_indicator(ee_product, image, already_normalized, sensitivity=1.0):
     red, nir= get_bands_by_name(ee_product, image, ['Red', 'NIR'])
     raw = (red - nir) / (red + nir + 1e-9)
     return raw * 2 + .7  # Scale to fit 1,2 thresholds, .15-->1, .65-->2
 
 
-def get_nbr_indicator(ee_product, image, sensitivity=1.0):
+def get_nbr_indicator(ee_product, image, already_normalized, sensitivity=1.0):
     nir, swir2 = get_bands_by_name(ee_product, image, ['NIR', 'SWIR2'])
     return sensitivity*(nir - swir2)/(nir + swir2 + 1e-9)
 
@@ -106,11 +116,9 @@ def vis_from_indicator(ind_func, l_func, comp_image=None):
     def handler(ee_product, image, comp_image=comp_image):
         B, G, R, nir, swir, swir2 = get_bands_by_name(ee_product, image,
                                                       ['Blue', 'Green', 'Red', 'NIR', 'SWIR', 'SWIR2'])
-        #ind_arrays = get_bands_by_name(ee_product, image, ind_bands)
-        index = ind_func(ee_product,image)
+        index = ind_func(ee_product,image,already_normalized=True)
         if comp_image is not None:
-            #comp_ind_arrays = get_bands_by_name(ee_product, comp_image, ind_bands)
-            comp_index = ind_func(ee_product, comp_image)
+            comp_index = ind_func(ee_product, comp_image,already_normalized=True)
             index = index - comp_index
         some_array, lots_array = l_func(B, G, R, nir, swir2)
         no_array = get_natural_nirswirmix(B, G, R, nir, swir2)
@@ -120,21 +128,6 @@ def vis_from_indicator(ind_func, l_func, comp_image=None):
         return combined_array
 
     return handler
-
-
-def get_conf_nbr(ee_product, image):
-    nir, swir, alpha = get_bands_by_name(ee_product, image, ['NIR', 'SWIR', 'cloud_mask'])
-    return (nir - swir) / (nir + swir + 1e-9)
-
-
-def get_conf_fire(ee_product, image):
-    swir, swir2 = get_bands_by_name(ee_product, image, ['SWIR', 'SWIR2'])
-    return get_fire_indicator(swir, swir2)
-
-
-def get_conf_firethresh(ee_product, image):
-    swir, swir2 = get_bands_by_name(ee_product, image, ['SWIR', 'SWIR2'])
-    return swir + swir2 / 4
 
 
 vis_veg = vis_from_indicator(ind_func=get_veg_indicator,
@@ -151,12 +144,3 @@ vis_fire = vis_from_indicator(ind_func=get_fire_indicator,
 vis_dnbr = vis_from_indicator(ind_func=partial(get_nbr_indicator,sensitivity=50),
                               l_func=get_fire_levels)
 
-
-@vis_handler_wrapper
-def vis_firethresh(ee_product, image):
-    swir, swir2, mask = get_bands_by_name(ee_product, image, ['SWIR', 'SWIR2', 'cloud_mask'])
-    out = apply_palette((swir + swir2) / 4, [
-        'black', 'red', 'yellow'
-    ])
-    out[-1] = mask
-    return out
